cb7de7a713e152efa1af943e8cc7d3ff26424bc4
Peter Horvath <restapi.solutons@gmail.com>
2021-06-16 20:02:12 +0100

    added optimizer to reduce contract size!

--- hardhat.config.js
+++ hardhat.config.js
@@ -23,9 +23,21 @@ module.exports = {
     compilers: [
       {
         version: "0.8.0",
+        settings:{
+          optimizer:{
+            enabled: true,
+            runs:1000
+          }
+        }
       },
       {
         version: "0.8.5",
+         settings:{
+          optimizer:{
+            enabled: true,
+            runs:1000
+          }
+        }
       },
     ],
   },
