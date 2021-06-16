edcd66d542a670f602d91fc47840363136f8bca8
Peter Horvath <restapi.solutons@gmail.com>
2021-06-16 16:05:29 +0100

    new config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("hardhat-contract-sizer");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-solhint");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
@@ -20,13 +22,21 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.0"
-      
+        version: "0.8.0",
       },
       {
-        version: "0.8.5"
-      }
-    ]
+        version: "0.8.5",
+      },
+    ],
   },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
+   gasReporter: {
+    currency: 'CHF',
+    gasPrice: 21,
+    enabled : true
+  }
 };
-
