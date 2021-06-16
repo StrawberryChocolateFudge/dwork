e517c55669ce8c405f9bb77883a85df6d6c7d1a5
Peter Horvath <restapi.solutons@gmail.com>
2021-06-16 19:40:54 +0100

    refactoring,fallback,assign job removed etc

--- contracts/WorkSpace.sol
+++ contracts/WorkSpace.sol
@@ -8,11 +8,14 @@ import "./WorkSpaceLib.sol";
 import "./CloneFactory.sol";
 import "./Initializer.sol";
 import "./WorkSpaceFactory.sol";
+
+
 contract WorkSpace is AccessControl, CloneFactory,Initializable{
     event RegistrationSuccess(bytes32 role, address registeredAddress);
-    event ModerationResult(bool done);
-    event CreateJobCalled(bool called,string msg);
-    
+
+    //TODO: ADD events to tests!!
+    event JobCreated();
+    event FallbackTriggered(address);
     using WorkSpaceLib for WorkSpaceState;
     WorkSpaceState state;
 
@@ -25,11 +28,8 @@ contract WorkSpace is AccessControl, CloneFactory,Initializable{
         uint workSpaceVersion,
         uint jobVersion
     ) external initializer() {
-        
-        // the manager must deploy his own contract to match Jobs
-        require(_manager != address(0), "_manager is the zero address");
-        require(_jobLibraryAddress != address(0), "_jobLibraryAddress is the zero address");
-
+        require(_manager != address(0), "504");
+        require(_jobLibraryAddress != address(0), "505");
         state.fee = _fee;
         state.metadataUrl = _metadataUrl;
         state.managerAddress = payable(_manager);
@@ -40,55 +40,65 @@ contract WorkSpace is AccessControl, CloneFactory,Initializable{
         state.jobVersion = jobVersion;
 
         _setupRole(RoleLib.MANAGER_ROLE, _manager);
-        // initializer PROTECTION!
-        // lets call the factory with the msg.sender address!
+        // lets call the factory with the msg.sender address, an Abi call to avoid getting called by a non factory
         WorkSpaceFactory factory = WorkSpaceFactory(msg.sender);
         bool isReal = factory.amIAFactory();
-        require(isReal,"Caller is not the factory!");
+        require(isReal,"506");
     }
 
 
     function createJob() external onlyRole(RoleLib.CLIENT_ROLE) {
-        require(state.clients[msg.sender].initialized == true, "The client must be initialized");
-        require(state.clients[msg.sender].disabled == false, "Disabled clients cannot create jobs");//TODO: TEST 
-        //TODO: LOCK!
+        require(state.clients[msg.sender].initialized == true, "507");
+        require(state.clients[msg.sender].disabled == false, "508");
+        require(state.lock[msg.sender] == false,"519");
+        state.lock[msg.sender] = true;
         
-        Job job = Job(createClone(state.jobLibraryAddress));
-        job.initialize(state.factoryAddress,address(this),msg.sender);
+        Job job = Job(payable(createClone(state.jobLibraryAddress)));
+
+        try job.initialize(state.factoryAddress,address(this),msg.sender){
         state.clientjobs[msg.sender].push(job);
+        emit JobCreated();
+        } catch {
+            state.lock[msg.sender] = false;
+
+        }
+
+        state.lock[msg.sender] = false;
+
     }
 
      
-     function AssignWorkers(address to, address[] calldata workerAddresses,uint[] calldata shares,string calldata _metadataUrl) external {
-         //TODO: test THIS!
-         bool isManager = hasRole(RoleLib.MANAGER_ROLE, msg.sender);
-         bool isClient = hasRole(RoleLib.CLIENT_ROLE,msg.sender);
-         require(isManager || isClient,"Sender must be manager or client");
-         require(workerAddresses.length == shares.length,"The address and share array must match!");
-         
-         for(uint i = 0;i < workerAddresses.length;i++){
-          Job job = Job(to);
-          //require that the worker address exists
-          //job.createAssignment(a,zs shares_,_metadataUrl);
-          // add the worker to the worker jobs
-         }
-     }
+    // function assignWorkers(address to, address[] calldata workerAddresses,uint[] calldata shares,string calldata _metadataUrl) external {
+    //      //TODO: test THIS!
+    //      bool isManager = hasRole(RoleLib.MANAGER_ROLE, msg.sender);
+    //      bool isClient = hasRole(RoleLib.CLIENT_ROLE,msg.sender);
+    //      require(isManager || isClient,"509");
+    //      require(workerAddresses.length == shares.length,"510");
+    //      //TODO: Lock!
 
-    // TODO: Assign workers to jobs through the workspace!
 
+    //      for(uint i = 0;i < workerAddresses.length;i++){
+    //       Job job = Job(payable(to));
+    //       //require that the worker address exists
+    //       //job.createAssignment(a,zs shares_,_metadataUrl);
+    //       // add the worker to the worker jobs
+    //      }  
+
+
+    //  }
     function registerWorker(
         string calldata _metadataUrl,
         address workerAddress,
         string calldata inviteToken,
         bytes32 _writtenContractHash
-    ) external {
-        require(workerAddress != address(0), "workerAddress is the zero address");
-        require(hasRole(RoleLib.MANAGER_ROLE,workerAddress) == false,"The manager cannot become a worker.");
+    ) external returns (Worker memory) {
+        require(workerAddress != address(0), "511");
+        require(hasRole(RoleLib.MANAGER_ROLE,workerAddress) == false,"512");
         if (state.managerAddress != msg.sender){
            require(workerAddress == msg.sender);
         } 
-        require(state.registrationOpen,"Registration is not open");
-        require(state.workers[msg.sender].initialized == false,"The worker already signed up");
+        require(state.registrationOpen,"513");
+        require(state.workers[msg.sender].initialized == false,"514");
 
         state.registerWorker(
             _metadataUrl,
@@ -97,6 +107,7 @@ contract WorkSpace is AccessControl, CloneFactory,Initializable{
             _writtenContractHash
         );
         _setupRole(RoleLib.WORKER_ROLE, workerAddress);
+        return state.workers[workerAddress];
     }
 
     function registerClient(
@@ -105,16 +116,15 @@ contract WorkSpace is AccessControl, CloneFactory,Initializable{
         string calldata inviteToken,
         bytes32 _writtenContractHash
     ) external returns (Client memory) {
-        require(clientAddress != address(0), "clientAddress is the zero address");
-        require(hasRole(RoleLib.MANAGER_ROLE,clientAddress) == false,"The client cannot become a manager.");
+        require(clientAddress != address(0), "515");
+        require(hasRole(RoleLib.MANAGER_ROLE,clientAddress) == false,"516");
         if (state.managerAddress != msg.sender){
             // If it's not the manager sending this transaction then the address has to be the sender
             // It's because a manager can sign up other people.
           require(clientAddress == msg.sender);
         } 
-        
-        require(state.registrationOpen,"Registration is not open");
-        require(state.clients[msg.sender].initialized == false,"The client already signed up"); 
+        require(state.registrationOpen,"517");
+        require(state.clients[msg.sender].initialized == false,"518"); 
         state.registerClient(
             _metadataUrl,
             clientAddress, 
@@ -254,5 +264,13 @@ contract WorkSpace is AccessControl, CloneFactory,Initializable{
 
     function getVersions() external view returns (uint,uint){
         return (state.workSpaceVersion,state.jobVersion);
+    }
+
+    function getAddresses() external view returns (address[] memory,address[] memory){
+        return state.getAddresses();
+    }
+
+    fallback() external {
+        emit FallbackTriggered(msg.sender);
     }
 }
