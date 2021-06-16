cd75484d8822b9754574a5f72c585a7b5dbbff34
Peter Horvath <restapi.solutons@gmail.com>
2021-06-16 16:03:59 +0100

    added a new role

--- contracts/RoleLib.sol
+++ contracts/RoleLib.sol
@@ -6,4 +6,5 @@ library RoleLib {
     bytes32 public constant WORKER_ROLE = keccak256("WORKER_ROLE");
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
     bytes32 public constant CLIENT_ROLE = keccak256("CLIENT_ROLE");
+    bytes32 public constant WORKSPACE = keccak256("WORKSPACE");
 }
