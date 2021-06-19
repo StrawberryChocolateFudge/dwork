24a0b7b6c8f697cb5bedf6a4fab03ceb221b2bc7
Peter Horvath <restapi.solutons@gmail.com>
2021-06-19 20:57:16 +0100

    added methods

--- contracts/JobLib.sol
+++ contracts/JobLib.sol
@@ -6,33 +6,46 @@ struct JobState {
     address workspaceAddress; // The address of the workspace that created the job
     //to get fees and manager address
     address clientAddress; // the address of the client
+    address dividendsContract;
     uint256 created;
     bool disabled;
-  
     //There is only one assignment active at a time, the last
     mapping(uint32 => Assignment) assignments;
     uint32 lastAssignment;
     mapping(uint32 => address payable) assignee; // this is like snapshots of assignee data accessed with a hashs
     uint32 lastAssignee;
     string metadataUrl;
-
     uint32 version;
+    uint256 totalBalance;
+    uint8 managementFee;
+    uint8 contractFee;
 }
 
 struct Assignment {
     uint256 created;
     bool initialized; // check if the assignment exists with this
+    bool ready; //Ready means work can start, if this is false self destruct is available
     bool startedWork; // marked by the worker, if work started the client or manager cannot add a new worker
-    bool done; // done is marked by the worker
-    bool disputeRequested; //can be requested by the client to ask the manager to help him withdraw
-    bool accepted; // marked by the client or the manager depending if there was and the result of the dispute
-    uint payed; // The amount that was payed
+    bool done; // done is marked by the worker when he is finished
+    uint256 finalPrice; //The final amount that is payed out, determined when the job is DONE
+    bool accepted; // marked by the client or the manager depending if there was a dispute and what was the result, when this is true, withdrawals can start to the worker
+    bool disputeRequested; //can be requested by the client to ask the manager to help him withdrawz his funds
+    bool refundAllowed; //It can be refunded if the client requests a dispute
+    uint256 workerPayed; // The amount that was payed to the worker
+    uint256 managerPayed; // The amount that was payed to the manager
+    uint256 feePayed; // The amount that was payed to the dividends contract
+    uint256 refundPayed; // IF THE REFUND IS ALLOWED 
 }
 
 library JobLib {
-    event AssigneeAdded(address _address);
-    uint256 public constant totalShares = 1000000;
+    event AssignmentAdded(bool ready);
+    event AssignmentReady(bool ready);
+    event WorkStarted(uint256 date);
+    event WorkDone(uint256 date);
+    event DisputeRequested(uint256 date);
+    event AssignmentAccepted(uint256 date);
 
+    uint256 public constant totalShares = 1000000;
 
     function addWorker(JobState storage self, address worker)
         external
@@ -40,41 +53,122 @@ library JobLib {
     {
         require(worker != address(0), "worker is the zero address");
         //TODO Test triggering the bellow require
-        require(self.assignments[self.lastAssignment].startedWork == false,"Cannot assign new worker if work has started");
+        require(
+            self.assignments[self.lastAssignment].startedWork == false,
+            "Cannot assign new worker if work has started"
+        );
         self.lastAssignee++;
         self.assignee[self.lastAssignee] = payable(worker);
         return true;
     }
 
+    function validatePayouts(JobState storage self)
+        internal
+        view
+        returns (bool)
+    {
+        return
+            self.assignments[self.lastAssignment].workerPayed > 0 &&
+            self.assignments[self.lastAssignment].managerPayed > 0 &&
+            self.assignments[self.lastAssignment].feePayed > 0;
+    }
 
+    function addAssignment(JobState storage self, bool ready) external {
+        if (self.assignments[self.lastAssignment].initialized) {
+            require(
+                validatePayouts(self)||
+                    self.assignments[self.lastAssignment].refundAllowed,
+                "The last assignment's payments must be done or a refund must be issued"
+            );
+            // If the assignment is refunded, the client can decide to not withdraw his money,
+            // he can instead create a new assignment and use the funds for that.
+            // he can assign a new worker if the new assignment is submitted as not ready.
+        }
+        self.lastAssignment++;
+        self.assignments[self.lastAssignment] = Assignment({
+            created: block.timestamp,
+            initialized: true,
+            ready: ready,
+            startedWork: false,
+            done: false,
+            finalPrice : 0,
+            disputeRequested: false,
+            accepted: false,
+            refundAllowed: false,
+            workerPayed: 0,
+            managerPayed: 0,
+            feePayed: 0,
+            refundPayed: 0
+        });
+        emit AssignmentAdded(ready);
+    }
 
+    function markReady(JobState storage self) external {
+        require(
+            self.assignments[self.lastAssignment].initialized,
+            "The assignment doesn't exist"
+        );
 
-    // Assignment memory _assignment = Assignment({
-    //     created: block.timestamp,
-    //     initialized : true,
-    //     done: false,
-    //     disputeRequested : false,
-    //     accepted : false
-    //     });
+        require(
+            self.assignments[self.lastAssignment].ready == false,
+            "The assignment is already ready"
+        );
+        self.assignments[self.lastAssignment].ready = true;
+        emit AssignmentReady(true);
+    }
 
-    // You can create an assignment without any asssignees
+    //TODO TEST!
+    function startWork(JobState storage self) external {
+        require(
+            self.assignments[self.lastAssignment].initialized,
+            "The assignment doesn't exist"
+        );
+        require(
+            self.assignments[self.lastAssignment].ready,
+            "The assignment is not ready yet"
+        );
+        require(
+            self.assignments[self.lastAssignment].startedWork == false,
+            "The work started already"
+        );
+        self.assignments[self.lastAssignment].startedWork = true;
+        emit WorkStarted(block.timestamp);
+    }
 
-    // if(assignees_.length > 0){
-    //   for (uint256 i = 0; i < assignees_.length;i++){
-    //    require(share_[i] <= totalShares,"invalid share can be max 1 million");
-    //    allSentShares += share_[i];
-    //    Assignee memory createdAssignee = _createAssignee(assignees_[i],share_[i]);
-    //    bytes32 hashedAssignee = keccak256(abi.encodePacked(self.round,createdAssignee.assigneeAddress));
+    function markDone(JobState storage self) external {
+        require(
+            self.assignments[self.lastAssignment].startedWork,
+            "The work didn't start yet."
+        );
+        self.assignments[self.lastAssignment].done = true;
+        emit WorkDone(block.timestamp);
+    }
 
-    //    _assignment.assigneeHashes[i] = hashedAssignee;
-    //    self.assignees[hashedAssignee] = createdAssignee;
+    function disputeRequested(JobState storage self) external {
+        self.assignments[self.lastAssignment].disputeRequested = true;
+        emit DisputeRequested(block.timestamp);
+    }
 
-    //    emit AssigneeAdded(assignees_[i],share_[i]);
+    function resolveDispute(JobState storage self, bool refundAllowed)
+        external
+    {
+        require(
+            self.assignments[self.lastAssignment].disputeRequested,
+            "No dispute to resolve"
+        );
+        //The manager can turn off the dispute and decide to allow refund or not
+        //if the refund is allowed, the dispute state persists
+        self.assignments[self.lastAssignment].disputeRequested = refundAllowed;
+        self.assignments[self.lastAssignment].refundAllowed = refundAllowed;
+    }
 
-    //   }
-    //aa  }
+    function markAccepted(JobState storage self) external {
+        require(
+            self.assignments[self.lastAssignment].done,
+            "The work is not done, yet."
+        );
+        self.assignments[self.lastAssignment].accepted = true;
+        emit AssignmentAccepted(block.timestamp);
 
-    //    require(allSentShares <= totalShares,"the assignees have too many shares");
-    //    self.assignments[self.round] = _assignment;
-    // }
+    }
 }
