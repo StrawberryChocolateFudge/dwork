607af7b5b28982a76750810c158b57d9b5c30f41
Peter Horvath <restapi.solutons@gmail.com>
2021-06-21 15:22:33 +0100

    added interface definitions for future upgrades

--- contracts/IJob.sol
+++ contracts/IJob.sol
@@ -1,5 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.5;
 
+/*
+  dev comment: This interface defines the functions that 
+  need to be included in a job contract. 
+  All future Job libs must implement this interface, to be able to be used by the factory
+  */
+interface IJob {
+    /*
+    This is called by the factory to initialize the job
+    */
 
-interface IJob{}
+    function initialize(
+        address _workSpaceAddress,
+        address _clientAddress,
+        address _managerAddress,
+        string calldata metadataUrl,
+        uint32 version,
+        uint16 contractFee,
+        uint16 managementFee,
+        address dividendsContract
+    ) external;
+
+    /*
+    Add worker is called by the workspace, to add a worker
+    */
+    function addWorker(address workerAddress) external returns (bool);
+}
--- contracts/IWorkSpace.sol
+++ contracts/IWorkSpace.sol
@@ -1,7 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.5;
 
+/*
+  dev comment: This interface defines the functions that 
+  need to be included in a workspace contract. 
+  All future Workspace libs must implement this interface, to be able to be used by the factory
+  */
 
-interface IWorkSpace{
-	
+interface IWorkSpace {
+    /*
+	The workspace initializer as called by the factory
+	*/
+    function initialize(
+        uint16 _fee,
+        string memory _metadataUrl,
+        address _manager,
+        uint256 workSpaceVersion
+    ) external;
+
+    /*
+     The job creation function that calls the workspace inside it
+     It must call factory.createJob
+    */
+    function createJob(string calldata _metadataUrl) external;
+
+    /*
+     addWorker must call the job to add a worker to it.
+    */
+
+    function addWorker(address to, address workerAddress) external;
 }
