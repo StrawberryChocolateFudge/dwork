58694d2ff896b4cdc6ff826fa30641ae260fa726
Peter Horvath <restapi.solutons@gmail.com>
2021-06-22 17:54:07 +0100

    added a counter

--- contracts/FactoryContractVerifier.sol
+++ contracts/FactoryContractVerifier.sol
@@ -4,10 +4,14 @@ import "./WorkSpaceFactory.sol";
 import "hardhat/console.sol";
 
 struct FactoryContractVerifierState{
-  uint version;
+  uint256 count;
+
 }
 library FactoryContractVerifier{
-  function checkFactoryBytecode(FactoryContractVerifierState storage self,address factoryAddress) external view returns (bool) {
+  event Verification(uint256 count);
+  function checkFactoryBytecode(FactoryContractVerifierState storage self,address factoryAddress) external returns (bool) {
+        self.count++;
+        emit Verification(self.count);
         bytes32 runtimeCodeHash = keccak256(type(WorkSpaceFactory).runtimeCode);
         bytes32 callerCodeHash;
         assembly{
