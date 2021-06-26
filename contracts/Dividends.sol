759e6be658920f9e12acce3e47d7667db4aa7d45
Peter Horvath <restapi.solutons@gmail.com>
2021-06-26 19:26:39 +0100

    BoardCallable interface added

--- contracts/Dividends.sol
+++ contracts/Dividends.sol
@@ -8,8 +8,10 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/BoardCallableDividends.sol";
 
-contract Dividends is Initializable, ReentrancyGuard, Ownable {
+
+contract Dividends is Initializable, ReentrancyGuard, Ownable,BoardCallableDividends {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
     using DividendsLib for DividendsState;
@@ -117,7 +119,7 @@ contract Dividends is Initializable, ReentrancyGuard, Ownable {
     }
 
     function withdrawDifference(address to)
-        external
+        external override
         nonReentrant
         onlyOwner
         returns (uint256 difference)
