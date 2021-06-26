26996c67897737f370c5ed589795a3d03de4477e
Peter Horvath <restapi.solutons@gmail.com>
2021-06-26 19:44:19 +0100

    Refactored to more efficient

--- contracts/FactoryContractVerifier.sol
+++ contracts/FactoryContractVerifier.sol
@@ -9,17 +9,18 @@ struct FactoryContractVerifierState {
 library FactoryContractVerifier {
     event Verification(uint256 count);
 
+    bytes32 constant codeHash = keccak256(type(WorkSpaceFactory).runtimeCode);
+
     function checkFactoryBytecode(
         FactoryContractVerifierState storage self,
         address factoryAddress
     ) external returns (bool) {
         self.count++;
         emit Verification(self.count);
-        bytes32 runtimeCodeHash = keccak256(type(WorkSpaceFactory).runtimeCode);
         bytes32 callerCodeHash;
         assembly {
             callerCodeHash := extcodehash(factoryAddress)
         }
-        return runtimeCodeHash == callerCodeHash;
+        return codeHash == callerCodeHash;
     }
 }
